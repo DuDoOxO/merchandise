43f86adee1e42cdbe3a51c3c341c2cc8cce92245
Enoch <ax6a9327@gmail.com>
2021-08-13 12:20:11 +0800

    ✨ feat(add merchandise update condition models):

--- models/merchandise.go
+++ models/merchandise.go
@@ -27,6 +27,25 @@ type Merchandise struct {
 	UpdatedAt   time.Time `json:"updated_at" xorm:"updated"`
 }
 
+type MerchandiseCondition struct {
+	Name        string    `json:"name"`
+	UPrice      int64     `json:"u_price"`
+	LPrice      int64     `json:"l_price"`
+	Launched    int64     `json:"launched"`
+	StartOfSale time.Time `json:"start_of_sale"`
+	EndOfSale   time.Time `json:"end_of_sale"`
+}
+
+type MerchandiseUpdate struct {
+	Name        string    `json:"name" xorm:"not null" validate:"required"`
+	Cost        int64     `json:"cost"`
+	Price       int64     `json:"price" validate:"gtfield=Cost"`
+	Statement   string    `json:"statement" xorm:"not null" validate:"required"`
+	Launched    int64     `json:"launched" xorm:"default 0"`
+	StartOfSale time.Time `json:"start_of_sale" xorm:"not null" validate:"required"`
+	EndOfSale   time.Time `json:"end_of_sale" xorm:"not null" validate:"required,gtefield=StartOfSale"`
+}
+
 type CatalogLinkMerchandise struct {
 	Id            int64     `json:"id" xorm:"pk autoincr not null"`
 	LayerRoot     int64     `json:"layer_root" xorm:"default 0"`
