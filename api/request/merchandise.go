397fa9ba0e7cd4b625fa69691bc094148d6109cf
Enoch <ax6a9327@gmail.com>
2021-08-22 15:46:12 +0800

    ✨ feat(add api request and response of merchandise definition):

--- api/request/merchandise.go
+++ api/request/merchandise.go
@@ -1,5 +1,7 @@
 package request
 
+import "time"
+
 type MerchandiseCatalog struct {
 	Name string `json:"name"`
 }
@@ -7,4 +9,25 @@ type MerchandiseCatalog struct {
 type MerchandiseCatalogUpdate struct {
 	Name   string `json:"name"`
 	Hidden int64  `json:"hidden"`
+	PrevId int64  `json:"prev_id"`
+	IsRoot int64  `json:"is_root"`
+}
+
+type Merchandise struct {
+	Name        string    `json:"name"`
+	Cost        int64     `json:"cost"`
+	Price       int64     `json:"price"`
+	Statement   string    `json:"statement"`
+	Launched    int64     `json:"launched"`
+	StartOfSale time.Time `json:"start_of_sale"`
+	EndOfSale   time.Time `json:"end_of_sale"`
+}
+
+type MerchandiseCondition struct {
+	Name        string    `json:"name"`
+	UPrice      int64     `json:"u_price"`
+	LPrice      int64     `json:"l_price"`
+	Launched    int64     `json:"launched"`
+	StartOfSale time.Time `json:"start_of_sale"`
+	EndOfSale   time.Time `json:"end_of_sale"`
 }
--- api/response/merchandise.go
+++ api/response/merchandise.go
@@ -9,3 +9,14 @@ type MerchandiseCatalog struct {
 	CreatedAt time.Time `json:"created_at" example:"2020-10-20 18:18:10"`
 	UpdatedAt time.Time `json:"updated_at" example:"2020-10-21 20:18:10"`
 }
+
+type Merchandise struct {
+	Id          int64       `json:"id" xorm:"pk autoincr not null"`
+	Name        string      `json:"name" xorm:"not null" validate:"required"`
+	Cost        int64       `json:"cost"`
+	Price       int64       `json:"price" validate:"gtfield=Cost"`
+	Statement   string      `json:"statement" xorm:"not null" validate:"required"`
+	Launched    int64       `json:"launched" xorm:"default 0"`
+	StartOfSale time.Time   `json:"start_of_sale" xorm:"not null" validate:"required"`
+	EndOfSale   time.Time   `json:"end_of_sale" xorm:"not null" validate:"required,gtefield=StartOfSale"`
+}
